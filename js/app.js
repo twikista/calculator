12f08d6ef8e3abbdaa315258515d73bd906fd092
viro <twikista@gmail.com>
2021-10-17 01:12:33 +0100

    refacored code and added clear functionality

--- js/app.js
+++ js/app.js
@@ -80,85 +80,152 @@ let operator = "";
 let solution = "";
 let currentOperand = "";
 const arr = [];
+let pressedKey = "";
+let equalitySign = "";
+let screenTopRow = document.querySelector(".screen-top-row");
+let screenBottomRow = document.querySelector(".screen-bottom-row");
 const keys = document.querySelectorAll("button");
 console.log(keys);
 
 document.addEventListener("keyup", (e) => {
   const target = e.key;
   keys.forEach((key) => {
-    if (target === key.dataset.numberkey) {
+    if (
+      key.classList.contains("number-key") &&
+      target === key.dataset.numberkey
+    ) {
+      const numberKeys = key.dataset.numberkey;
+      pressedKey = target;
+      currentOperand += pressedKey;
+      console.log(numberKeys);
       console.log(target);
-      calculatorScreenDisplay(target);
+      calculatorScreenDisplay(numberKeys);
       setOperandTwoVAlue(currentOperand);
-      return target;
-    } else if (target === key.dataset.functionalkey) {
-      setOperandOneValue(target, currentOperand);
+      setSolutuion();
+    } else if (
+      key.classList.contains("operator-key") &&
+      target === key.dataset.functionalkey
+    ) {
+      const functionalKeys = key.dataset.functionalkey;
+      setOperandOneValue(currentOperand);
 
       console.log(target);
       operator = target;
+      console.log(operator);
       currentOperand = "";
-      calculatorScreenDisplay(target, operator);
-      return target;
-    } else if (target === key.dataset.equalskey) {
+      calculatorScreenDisplay(functionalKeys);
+    } else if (
+      key.classList.contains("equals-key") &&
+      target === key.dataset.equalskey
+    ) {
       const equalsKey = key.dataset.equalskey;
-      solution = operate(operandOne, operator, operandTwo);
-      calculatorScreenDisplay(target, operator, equalsKey);
+      equalitySign = target;
+      console.log(pressedKey);
+      setSolutuion();
       console.log(solution);
+      calculatorScreenDisplay(equalsKey);
       currentOperand = "";
+      arr.length = 0;
+      operandOne = "";
+      operandTwo = "";
+    } else if (
+      key.classList.contains("clear-key") &&
+      target === key.dataset.clearkey
+    ) {
+      const clear = target;
+      const clearKey = key.dataset.clearkey;
+
+      currentOperand = "";
+      operandOne = "";
+      operandTwo = "";
+      solution = "";
+      arr.length = 0;
+      calculatorScreenDisplay(clearKey, clear);
+    } else if (key.classList.contains("backspace-key")) {
     }
   });
 });
 
-function calculatorScreenDisplay(pressedKey, operate, equals) {
-  let screenTopRow = document.querySelector(".screen-top-row");
-  let screenBottomRow = document.querySelector(".screen-bottom-row");
-  if (operandOne === "" && operandTwo === "" && solution !== "") {
-    screenTopRow.textContent = "";
-    solution = "";
-  }
-  screenTopRow.textContent += pressedKey;
-  currentOperand += pressedKey;
-  screenBottomRow.textContent = currentOperand;
-
-  if (pressedKey === operate) {
-    screenBottomRow.textContent = "";
-    currentOperand = "";
-  } else if (pressedKey === equals) {
+function calculatorScreenDisplay(keyType, clear) {
+  if (pressedKey === keyType) {
+    if (operandOne === "" && operandTwo === "" && solution !== "") {
+      screenTopRow.textContent = "";
+    }
+    screenTopRow.textContent += pressedKey;
+    console.log(screenTopRow.textContent);
+    screenBottomRow.textContent = currentOperand;
+    if (operandOne !== "" && operator === "") {
+      screenBottomRow.textContent = operandOne;
+    }
+  } else if (operator === keyType) {
+    console.log(pressedKey);
+    if (operandOne === "" && operandTwo === "" && solution !== "") {
+      screenTopRow.textContent = `${solution}`;
+      screenBottomRow.textContent = solution;
+    } else if (operandOne !== "" && operandTwo !== "") {
+      screenTopRow.textContent = `${solution}`;
+      screenBottomRow.textContent = solution;
+      console.log(screenTopRow.textContent);
+    }
+    screenTopRow.textContent += operator;
+    // screenBottomRow.textContent = "";
+    // currentOperand = "";
+  } else if (equalitySign === keyType) {
     screenBottomRow.textContent = solution;
-    screenTopRow.textContent = `${operandOne} ${operate} ${operandTwo}`;
-    arr.length = 0;
-    operandOne = "";
-    operandTwo = "";
-    currentOperand = "";
+    // screenTopRow.textContent = `${operandOne} ${operate} ${operandTwo}`;
+  } else if (clear === keyType) {
+    screenTopRow.textContent = "";
+    screenBottomRow.textContent = "";
   }
-  console.log(screenTopRow.textContent);
 }
 
 function setOperandTwoVAlue(presedValues) {
   operandTwo = +presedValues;
+  console.log(operandTwo);
   if (operandOne === "") {
     operandTwo = "";
-  } else if (operandOne !== "" && operator === "") {
+  }
+  if (operandOne !== "" && operator === "") {
+    operandOne += pressedKey;
     operandTwo = "";
   }
 }
 
-function setOperandOneValue(pressedKey, presedValues) {
-  if (operandOne !== "" && operator === "") {
-    operandOne += pressedKey;
-  } else if (operandOne === "" && operandTwo === "" && solution === "") {
+function setOperandOneValue(presedValues) {
+  if (operandOne === "" && operandTwo === "" && solution === "") {
     arr.push(+presedValues);
     operandOne = arr[0];
     console.log(arr);
     console.log(operandOne);
     console.log(presedValues);
+  } else if (operandOne === "" && operandTwo === "" && solution !== "") {
+    arr.length = 0;
+    arr.push(solution);
+    operandOne = arr[0];
+    console.log(operandOne);
+    operandTwo = "";
+    operator = "";
+  } else if (operandOne !== "" && operandTwo !== "") {
+    setSolutuion();
+    // solution = operate(operandOne, operator, operandTwo);
+    arr.length = 0;
+    arr.push(solution);
+    operandOne = arr[0];
+    // calculatorScreenDisplay();
+    operandTwo = "";
+    operator = "";
   }
 }
 
 function setSolutuion() {
+  solution = operate(operandOne, operator, operandTwo);
+
   if (operandOne === "" && operandTwo === "" && solution !== "") {
     solution = "";
   }
+  // else if (operandOne !== "" && operandTwo !== "") {
+  //   solution = operate(operandOne, operator, operandTwo);
+  // }
 }
 
 setSolutuion();
